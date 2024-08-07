22cc0c1f66039607085a0736c51bf0b5d90fe816
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2024-08-07 13:54:46 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,34 @@
+variable REGION {
+  default = "us-east-1"
+}
+
+
+variable VPC_NAME {
+  default = "VPC"
+}
+
+variable Zone {
+  default = "us-east-1a"
+}
+
+
+variable vpcCIDR {
+  default = "10.0.0.0/16"
+}
+
+
+variable PubSub1CIDR {
+  default = "10.0.101.0/24"
+}
+
+variable PrivSub1CIDR {
+  default = "10.0.1.0/24"
+}
+
+variable PubSub2CIDR {
+  default = "10.0.102.0/24"
+}
+
+variable PrivSub2CIDR {
+  default = "10.0.2.0/24"
+}
