3c266db8c90a3e7cc9c76f877a7be2ec9e783293
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2024-08-07 14:52:23 +0200

    Create task.tf

---
+++ task.tf
@@ -0,0 +1,18 @@
+data "aws_iam_role" "ecs_task_execution_role" { name = "ecsTaskExecutionRole" }
+resource "aws_ecs_task_definition" "this" {
+ container_definitions = jsonencode([{
+  environment: [
+   { name = "NODE_ENV", value = "production" }
+  ],
+  essential = true,
+  image = data.docker_image.local_image.name,
+  name = local.container_name,
+  portMappings = [{ containerPort = 8080 }],
+ }])
+ cpu = 256
+ execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+ family = "family-of-${local.example}-tasks"
+ memory = 512
+ network_mode = "awsvpc"
+ requires_compatibilities = ["FARGATE"]
+}
