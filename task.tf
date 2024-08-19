d310d27fad178d7f122ce7929903ec5a4cb549ff
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2024-08-19 13:26:32 +0200

    Update task.tf

--- task.tf
+++ task.tf
@@ -1,14 +1,14 @@
 data "aws_iam_role" "ecs_task_execution_role" { name = "ecsTaskExecutionRole" }
 resource "aws_ecs_task_definition" "my_task_definition" {
- container_definitions = jsonencode([{
-  image = "${aws_ecr_repository.my_repository.repository_url}:latest",
-  name = local.container_name,
-  portMappings = [{ containerPort = 8080 }],
- }])
- cpu = 256
- execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
- family = "family-of-${local.example}-tasks"
- memory = 512
- network_mode = "awsvpc"
- requires_compatibilities = ["FARGATE"]
+  family = "my-task-definition"
+  container_definitions = jsonencode([
+    {
+      name = "my-container"
+      image = "${aws_ecr_repository.my_repository.repository_url}:latest"
+      network_mode            = "bridge"
+      memory                  = 512
+      memory_reservation      = 256
+      
+    }
+  ])
 }
