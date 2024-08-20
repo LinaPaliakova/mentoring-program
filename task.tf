4065ed3d4452703d6f174b75a43e2af788ba75a9
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2024-08-20 14:12:46 +0200

    Update task.tf

--- task.tf
+++ task.tf
@@ -1,18 +1,18 @@
 data "aws_iam_role" "ecs_task_execution_role" { name = "ecsTaskExecutionRole" }
-resource "aws_ecs_task_definition" "this" {
+resource "aws_ecs_task_definition" "app-runner" {
  container_definitions = jsonencode([{
   environment: [
    { name = "NODE_ENV", value = "production" }
   ],
   essential = true,
-  image = resource.docker_registry_image.this.name,
+  image = "{aws_ecr_repository.my_repo.repository_url}:latest",
   name = local.container_name,
   portMappings = [{ containerPort = 8080 }],
  }])
- cpu = 256
+ cpu = 512
  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
  family = "family-of-${local.example}-tasks"
- memory = 512
+ memory = 1024
  network_mode = "awsvpc"
  requires_compatibilities = ["FARGATE"]
 }
