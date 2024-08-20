b38f61211d374b7443cbcb43d8769c652d6cdb19
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2024-08-20 13:46:59 +0200

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,20 @@
+module "ecr" {
+ source  = "terraform-aws-modules/ecr/aws"
+ version = "2.2.1"
+
+
+ repository_force_delete = true
+ repository_name = local.example
+ repository_lifecycle_policy = jsonencode({
+  rules = [{
+   action = { type = "expire" }
+   description = "Delete all images except a handful of the newest images"
+   rulePriority = 1
+   selection = {
+    countNumber = 3
+    countType = "imageCountMoreThan"
+    tagStatus = "any"
+   }
+  }]
+ })
+}
