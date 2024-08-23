0d8bcf6c0781e0acf9c469e8d1f81fce81fc46ee
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2024-08-23 13:33:51 +0200

    feat: Update app.py(version10)

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello palina, you are doing great  "
+    return "Hello palina, you are doing great! Have a nice daY"
 
 if __name__ == '__main__':
     app.run()
